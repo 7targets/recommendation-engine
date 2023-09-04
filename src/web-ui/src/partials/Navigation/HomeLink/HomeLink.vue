d3fbcd0fa8892a1cbdcda545a4020f42158d8ade
sanketgargate06 <sanketgargate06@gmail.com>
2023-09-04 12:11:08 +0530

    Web UI changes for Recommendation Engine

--- src/web-ui/src/partials/Navigation/HomeLink/HomeLink.vue
+++ src/web-ui/src/partials/Navigation/HomeLink/HomeLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link to="/" aria-label="home"><img src="/logo.svg" class="img-fluid"/></router-link>
+  <router-link to="/" aria-label="home"><h4>Recommendation Engine</h4></router-link>
 </template>
 
 <script>
